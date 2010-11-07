0b117ffb45db0e0c52b6390d8a0ad6ef50a52f07
talklittle <talklittle@52ab1de2-2072-ca69-adbc-2696a96684c3>
2010-11-07 21:04:43 +0000

    new version of push.sim

--- trunk/src/push/sim/Move.java
+++ trunk/src/push/sim/Move.java
@@ -6,6 +6,19 @@ public class Move {
 	private int x;
 	private int y;
 	private Direction direction;
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return x+y+direction.hashCode();
+	}
+	@Override
+	public boolean equals(Object arg0) {
+		// TODO Auto-generated method stub
+		if(!(arg0 instanceof Move))
+			return false;
+		else
+			return x == ((Move) arg0).x && y == ((Move) arg0).y && ((Move) arg0).direction == direction;
+	}
 	public int getNewX()
 	{
 		return x + direction.getDx();
