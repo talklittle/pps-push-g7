2b9f9c511c3ad14b14a4fb99a227f3c7c367ae00
talklittle <talklittle@52ab1de2-2072-ca69-adbc-2696a96684c3>
2010-11-12 16:50:07 +0000

    finish 10 round strategy, ready submit

--- trunk/src/push/g7/SimpleMoveStrategy.java
+++ trunk/src/push/g7/SimpleMoveStrategy.java
@@ -7,8 +7,8 @@ import java.util.Random;
 
 import org.apache.log4j.Logger;
 
-import push.sim.GameConfig;
 import push.sim.Move;
+import push.sim.MoveResult;
 import push.sim.Player.Direction;
 
 public class SimpleMoveStrategy {
@@ -19,7 +19,7 @@ public class SimpleMoveStrategy {
 	ArrayList<Direction> formerAllys = allys;
 
 	public Move generateHelpfulMove(int[][] board, Direction myCorner,
-			int round, RecognizeEnemyAndAlly recognize) {
+			int round, RecognizeEnemyAndAlly recognize, boolean isShortGame) {
 		logger.info("helpful move. round: " + round + "\n");
 		// Pick the ally to help
 		Direction allyToHelp;
@@ -36,11 +36,11 @@ public class SimpleMoveStrategy {
 		Collections.reverse(enemiesStrongToWeak);
 		priority.addAll(enemiesStrongToWeak);
 
-		return generalMove(0, round, board, myCorner, priority);
+		return generalMove(0, round, board, myCorner, priority, isShortGame);
 	}
 
 	public Move generateBetrayalMove(int[][] board, Direction myCorner,
-			int round, RecognizeEnemyAndAlly recognize) {
+			int round, RecognizeEnemyAndAlly recognize, boolean isShortGame) {
 		// find a betrayal move, i.e., large stacks that we can move to white
 		// spots
 		logger.info("betrayal move. round: " + round + "\n");
@@ -68,7 +68,7 @@ public class SimpleMoveStrategy {
 		for (int i = 0; i < scoresCopy.size(); i++) {
 			for (int id = 0; id < scores.size(); id++) {
 				if (scores.get(id) == scoresCopy.get(i)) {
-					if (!harmInvalid.contains(id)) {
+					if (!harmInvalid.contains(id) && !harmYesAllies.contains(id) && !harmNotAllies.contains(id)) {
 						if (allies.contains(recognize.playerPositions.get(id))) {
 							harmYesAllies.add(id);
 						} else {
@@ -88,10 +88,50 @@ public class SimpleMoveStrategy {
 		}
 
 		// supply a list of "to" Directions
-		return generalMove(1, round, board, myCorner, harmPriorityDirections);
+		return generalMove(1, round, board, myCorner, harmPriorityDirections, isShortGame);
 	}
 	
-	public Move helpOurselfMove(int[][]board,Direction myCorner,int round,int totalRounds,int formerScore, int afterScore,RecognizeEnemyAndAlly recognize)
+	/**
+	 * short games:
+	 * 1st round help opposite
+	 * remaining rounds help someone if they helped previous round (truncated history),
+	 * or else if no allies then help ourselves.
+	 * if we run out of moves to help ourselves (e.g. we had an ally helping for a while
+	 * but not previous round) then do a harmful move.
+	 * @param board
+	 * @param myCorner
+	 * @param round
+	 * @param previousMoves use only the previous round's MoveResults (only a 1-round memory)
+	 * @return
+	 */
+	public Move generateShortGameMove(int[][] board, Direction myCorner,
+			int round, int totalRounds, RecognizeEnemyAndAlly recognize, List<MoveResult> previousMoves,
+			int formerScore, int afterScore) {
+		if (round == 0) {
+			// help the Opposite
+			ArrayList<Direction> oppositeOnly = new ArrayList<Direction>();
+			oppositeOnly.add(myCorner.getOpposite());
+			return generalMove(0, round, board, myCorner, oppositeOnly, true);
+		} else {
+			ArrayList<Direction> allies = recognize.getAlliesStrongestToWeakest();
+			if (allies.isEmpty()) {
+				// No allies; Try to help ourselves (force it)
+				Move tryHelpOurself = helpOurselfMove(board, myCorner, round, totalRounds,
+						formerScore, afterScore, recognize, true);
+				if (tryHelpOurself != null) {
+					return tryHelpOurself;
+				}
+				// can't help ourselves so do a harmful move
+				return generateBetrayalMove(board, myCorner, round, recognize, true);
+			} else {
+				// we have ally from previous round, help them
+				return generateHelpfulMove(board, myCorner, round, recognize, true);
+			}
+		}
+	}
+	
+	public Move helpOurselfMove(int[][]board,Direction myCorner,int round,int totalRounds,
+			int formerScore, int afterScore,RecognizeEnemyAndAlly recognize, boolean force)
 	{
 		//last 3 round try the best to help ourself
 		if(round>=totalRounds-3)
@@ -106,7 +146,7 @@ public class SimpleMoveStrategy {
 
 			return singleAllyMove(board,myCorner,allys);
 		}
-		else if(formerScore>afterScore)
+		else if(formerScore>afterScore || force)
 		{
 			for(int y=0;y<StaticVariable.MAX_Y;y++)
 				for(int x=0;x<StaticVariable.MAX_X;x++)
@@ -127,11 +167,11 @@ public class SimpleMoveStrategy {
 
 
 	public Move generalMove(int status, int round, int[][] board,
-			Direction myCorner, ArrayList<Direction> allysOrEnemy) {
+			Direction myCorner, ArrayList<Direction> allysOrEnemy, boolean isShortGame) {
 		logger.info("ally : " + allys.iterator());
 		// in first 6 rounds, try to set up two allies with help them
 		// alternatively.
-		if (round < 2 * StaticVariable.TrySteps) {
+		if (round < 2 * StaticVariable.TrySteps && !isShortGame) {
 			allysOrEnemy.clear();
 			allysOrEnemy.add(myCorner.getRelative(0));
 			allysOrEnemy.add(myCorner.getRelative(1));
@@ -154,14 +194,13 @@ public class SimpleMoveStrategy {
 			// ally;
 			if (status == 0) {
 				// if set up keep allies with the two player.
-				if (formerAllys.contains(allysOrEnemy.get(0))
-						|| formerAllys.contains(allysOrEnemy.get(1))) {
+				if ((formerAllys.size() >= 1 && formerAllys.contains(allysOrEnemy.get(0)))
+						|| (formerAllys.size() >= 2 && formerAllys.contains(allysOrEnemy.get(1)))) {
 				    Move m = twoAlliesMove(board,myCorner,round, allysOrEnemy);
 					if (m != null) return m;
-				} else {
-					Move m = singleAllyMove(board,myCorner,allysOrEnemy);
-					if(m!=null){return m;}
 				}
+				Move m = singleAllyMove(board,myCorner,allysOrEnemy);
+				if(m!=null){return m;}
 			}
 			// enemy
 			else {
@@ -210,9 +249,13 @@ public class SimpleMoveStrategy {
 
 	public Move singleAllyMove(int[][] board,Direction myCorner, ArrayList<Direction> allysOrEnemy) 
 	{
+		if (allysOrEnemy.size() < 3)
+			return null;
+		
 		// try to help the first ally then the next. randomly help other allies.
 		Random rand = new Random();
 		ArrayList<Direction> allys = new ArrayList<Direction>();
+			
 		allys.add(allysOrEnemy.get(0));
 		allys.add(allysOrEnemy.get(1));
 		allys.add(allysOrEnemy.get(2));
@@ -234,6 +277,9 @@ public class SimpleMoveStrategy {
 	
 	public Move twoAlliesMove(int[][] board, Direction myCorner,int round, ArrayList<Direction> allysOrEnemy) 
 	{
+		if (allysOrEnemy.size() < 2)
+			return null;
+		
 		ArrayList<Direction> allys = new ArrayList<Direction>();
 		allys.add(allysOrEnemy.get(0));
 		allys.add(allysOrEnemy.get(1));
